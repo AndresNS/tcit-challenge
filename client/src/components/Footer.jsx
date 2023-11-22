c309631a864481b96cc3ef7171d8ae82c8e8197b
Andres Navarro S <andresm.navarro@gmail.com>
2023-11-22 17:38:51 -0300

    Add client project

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-6">
+      Developed by Andr&eacute;s Navarro S.
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/PostsTable.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostsTable = () => {
+  return (
+    <div className="bg-slate-100 text-black rounded shadow-sm p-4">
+      <table>table</table>
+    </div>
+  );
+};
+
+export default PostsTable;
