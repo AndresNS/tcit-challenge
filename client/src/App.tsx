6b382a959f1b1a7020f7380ed5c265bede77f3ad
Andres Navarro S <andresm.navarro@gmail.com>
2023-11-24 16:39:10 -0300

    Add header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,13 +6,15 @@ import Loader from "./components/ui/Loader";
 
 import { useSelector } from "react-redux";
 import { RootState } from "./state/store";
+import Header from "./components/sections/header";
 
 function App() {
   const loading = useSelector((state: RootState) => state.ui.loading);
 
   return (
-    <div className="bg-slate-700 text-white flex flex-col min-h-screen">
-      <div className="container mx-auto flex-1 flex flex-col gap-4">
+    <div className="bg-slate-600 text-white flex flex-col min-h-screen">
+      <Header />
+      <div className="container mx-auto mt-12 flex-1 flex flex-col gap-4">
         <PostsForm />
         <PostsTable />
       </div>
---
+++ client/src/components/sections/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="py-6 bg-slate-800">
+      <div className="container mx-auto text-2xl font-bold tracking-wide">
+        TCIT Challenge
+      </div>
+    </header>
+  );
+};
+
+export default Header;
